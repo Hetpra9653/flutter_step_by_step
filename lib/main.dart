8b63aaeca5bdee6cd6fcd557a8d684601b7cb562
het prajapati <prajapatihet9653@gmail.com>
2021-08-18 17:38:48 +0530

    message by stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -9,12 +9,22 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return new MaterialApp(
       title: "Myself",
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: new Text("Home page"),
-        ),
-        body: new Center(child: new Text("Hello World!,Hello")),
-      ),
+      home: new Homepage(),
     );
+  }
+}
+
+class Homepage extends StatefulWidget {
+  @override
+  _HomepageState createState() => new _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(title: new Text("Home page")),
+      body: new Center(child: new Text("Hello world!")),
+    ); //Widgets and other code her
   }
 }
