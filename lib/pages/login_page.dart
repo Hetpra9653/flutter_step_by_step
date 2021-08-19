db66ac8b552ad0d230f3165639f2f4930f2299dc
het prajapati <prajapatihet9653@gmail.com>
2021-08-20 01:04:10 +0530

    new logo page

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,18 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(
-        child: Text(
-          "Login Page",
-          style: TextStyle(
-            fontSize: 20,
-            color: Colors.blue,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
+      color: Colors.white,
+      child: Column(
+        children: [
+          Image.asset("assets/login_image.png"),
+          // SizedBox(
+          //   height: 01,
+          // ),
+          Text(
+            "Welcome To E-Fuel Filling App",
+            style: TextStyle(
+              fontSize: 20.0,
+              fontWeight: FontWeight.bold,
+              color: Colors.redAccent,
+            ),
+          )
+        ],
       ),
     );
   }
